f7b5e6707469c519cee49481cfe6983786ce4a59
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-12 04:40:38 +0100

    Airbnb v4 Task 6

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -54,53 +54,6 @@ $(document).ready(function () {
         }
     });
 
-    $.ajax({
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({}),
-        success: function(data) {
-            for (const place of data) {
-                const template = `<article>
-                    <div class="title_box">
-                    <h2>${place.name}</h2>
-                    <div class="price_by_night">
-                    
-                    $${place.price_by_night}
-                    </div>
-                    
-                    <div class="information">
-                    
-                    <div class="max_guest">
-                    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
-                    
-                     <br />
-                    Max ${place.max_guest} guests
-                    </div>
-                    <div class="number_rooms">
-                    
-                     <br />
-                    ${place.number_rooms} Room(s)
-                    </div>
-                    <div class="number_bathrooms">
-                    
-                     <br />
-                    ${place.number_bathrooms} Bathroom(s)
-                    </div>
-                    </div> <!--end of information-->
-                    
-                    <div class="description">
-                    ${place.description}
-                    </div>
-                    
-                    </div> <!--end of title_box-->
-                    
-                </article> <!--end of template article-->`;
-                $('section.places').append(template);
-            }
-        }
-    });
-
     $('button').click(function () {
         $.ajax({
             url: 'http://0.0.0.0:5001/api/v1/places_search/',
